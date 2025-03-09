6fb656117edd186a65cb87fa13e8dda54aa8eded
noeChicharro <noelia@chicharro.eu>
2025-03-09 14:43:08 +0100

    second Dataset

--- src/converter.py
+++ src/converter.py
@@ -2,11 +2,27 @@ import mysql.connector
 import pandas as pd
 from sqlalchemy import create_engine
 
-df = pd.read_csv('data/csv/finland.csv')
-print(df.dtypes)
 
-df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-print(df.head())
-
+# connect to database
 engine = create_engine('mysql+mysqlconnector://root:Wuschtel5!@localhost/expenditure')
-df.to_sql('valueYear', con=engine, if_exists='append', index=False)
+
+# read csv file
+finland = pd.read_csv('data/csv/finland.csv')
+print(finland.dtypes)
+
+# drop columns
+finland = finland.loc[:, ~finland.columns.str.contains('^Unnamed')]
+# save to csv
+finland.to_csv('data/csv/finland.csv', index=False)
+print(finland.head())
+
+# add to database (following code is commendted to not add the same data multiple times)
+#finland.to_sql('valueYear', con=engine, if_exists='append', index=False)
+
+spain = pd.read_csv('data/csv/spain.csv')
+spain = spain.round()
+
+spain = spain.loc[:, ~spain.columns.str.contains('^Unnamed')]
+print(spain.head())
+
+spain.to_sql('valueYear', con=engine, if_exists='append', index=False)
